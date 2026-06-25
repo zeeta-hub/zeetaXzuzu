dbd0c0be6f63b14ddaa49b74780ee0245b5f50d7
zeeta-hub <ega.argya@gmail.com>
2026-06-25 15:04:50 +0700

    Update GardenScript.lua

--- GardenScript.lua
+++ GardenScript.lua
@@ -49,7 +49,7 @@ titleText.BackgroundTransparency = 1
 titleText.Font = Enum.Font.GothamBold
 
 local discordBtn = Instance.new("TextButton", titleBar)
-discordBtn.Size = UDim2.new(50, 0, 0.15, 25)
+discordBtn.Size = UDim2.new(50, 50, 0.15, 25)
 discordBtn.Position = UDim2.new(0.8, 50, 0.15, 0)
 discordBtn.Text = "DISCORD"
 discordBtn.BackgroundColor3 = Color3.fromRGB(114, 137, 218)
